cd99a195f6c0e2f23dabfbb9671ae7110a6b31db
MichałN <michal.nicer@acaisoft.com>
2018-10-24 08:52:08 +0200

    add drawer to navigation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Navigation } from 'react-native-navigation';
 import { Component } from 'react';
 import { registerScreens } from './services/Screens';
 import store from './models';
-import SplashScreen from 'react-native-splash-screen';
+import Config from './services/Config';
 
 registerScreens(store, Provider);
 
@@ -11,23 +11,42 @@ export default class App extends Component<Props> {
   constructor(props) {
     super(props);
     store.subscribe(this.onStoreUpdate.bind(this));
+    this.rootScreen = {
+      view: '',
+    };
   }
 
   onStoreUpdate() {
     const { root } = store.getState().navigation;
-    if (root && this.rootScreen !== root) {
+    if (root && this.rootScreen.view !== root.view) {
       this.rootScreen = root;
-      this.switchRoot(root, 'Login');
+      this.switchRoot(root.view, root.title, root.sidebar);
     }
   }
 
-  switchRoot(root, title) {
-    Navigation.startSingleScreenApp({
-      screen: {
-        screen: root,
-        title,
-        navigatorStyle: {},
-      },
-    });
+  switchRoot(view, title, sidebar) {
+    if (sidebar) {
+      Navigation.startSingleScreenApp({
+        screen: {
+          screen: view,
+          title,
+          navigatorStyle: {},
+        },
+        drawer: {
+          left: {
+            screen: `${Config.urlPrefix}.Home`,
+          },
+          disableOpenGesture: true,
+        },
+      });
+    } else {
+      Navigation.startSingleScreenApp({
+        screen: {
+          screen: view,
+          title,
+          navigatorStyle: {},
+        },
+      });
+    }
   }
 }
--- src/models/app.js
+++ src/models/app.js
@@ -1,14 +1,23 @@
-import Config from '../services/Config';
+
 import SplashScreen from 'react-native-splash-screen';
+import Config from '../services/Config';
 
 const app = {
   effects: dispatch => ({
     async init() {
       const token = dispatch.auth.getToken(`${Config.urlPrefix}:token`);
       if (token) {
-        dispatch.navigation.setRoot(`${Config.urlPrefix}.Home`);
+        dispatch.navigation.setRoot({
+          view: `${Config.urlPrefix}.Home`,
+          title: 'Home',
+          sidebar: true,
+        });
       } else {
-        dispatch.navigation.setRoot(`${Config.urlPrefix}.Login`);
+        dispatch.navigation.setRoot({
+          view: `${Config.urlPrefix}.Login`,
+          title: 'Login',
+          sidebar: false,
+        });
       }
       SplashScreen.hide();
     },
