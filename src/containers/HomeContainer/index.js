4d4f83c413fdcfa1de2f8c8aebe1b6205f2739aa
MichałN <michal.nicer@acaisoft.com>
2018-10-24 09:00:48 +0200

    change home and home container

--- src/containers/HomeContainer/index.js
+++ src/containers/HomeContainer/index.js
@@ -1,61 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import {
-  Container,
-  Header,
-  Title,
-  Content,
-  Footer,
-  FooterTab,
-  Button,
-  Left,
-  Right,
-  Body,
-  Icon,
-  Text,
-} from 'native-base';
+
 import Home from './../../screens/Home';
 
 class HomeContainer extends React.Component<Props, State> {
-  // constructor(props, state) {
-  //   super(props, state);
-  //   console.log(this);
-  // }
-
   render() {
-    const { props } = this;
-    return (
-      <Container>
-        <Header>
-          <Left>
-            <Button transparent>
-              <Icon name="menu" />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Header</Title>
-          </Body>
-          <Right />
-        </Header>
-        <Content>
-          <Home {...props} />
-        </Content>
-        <Footer>
-          <FooterTab>
-            <Button full>
-              <Text>Footer</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
-    );
+    const { navigator } = this.props;
+    return <Home navigator={navigator} />;
   }
 }
 
-const mapStateToProps = state => ({});
-
 export default connect(
-  mapStateToProps,
+  null,
   null
 )(HomeContainer);
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,14 +1,63 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import {
+  Body,
+  Button,
+  Container,
+  Content,
+  Footer,
+  FooterTab,
+  Header,
+  Icon,
+  Left,
+  Right,
+  Title,
+} from 'native-base';
+import PropTypes from 'prop-types';
 
 // import styles from './styles';
 
 class Home extends React.Component<Props, State> {
+  static propTypes = {
+    navigator: PropTypes.object,
+  };
+
+  toggleMenu = () => {
+    const { navigator } = this.props;
+    navigator.toggleDrawer({
+      backButtonHidden: true,
+      side: 'left',
+      animated: true,
+    });
+  };
+
   render() {
     return (
-      <View>
-        <Text>Home 1</Text>
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent onPress={this.toggleMenu}>
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <View>
+            <Text>Home 1</Text>
+          </View>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Footer</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
     );
   }
 }
